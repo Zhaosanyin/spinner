4be74b2afa0e1055f87c7dc0bf34eb7d97b92854
zhaosanyin <zhaosan.yin@gmail.com>
2023-04-17 23:28:49 +0000

    spinner1 \r

---
+++ spinner1.js
@@ -0,0 +1,36 @@
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 200);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 400);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 600);
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 800);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1000);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1200);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 1400);
+setTimeout(() =>{
+  process.stdout.write('\n');
+}, 1600);
